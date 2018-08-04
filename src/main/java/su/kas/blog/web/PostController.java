5ea488bc4c087b1fc3320d6b72e72c67dea612d9
xtal <kasuhanov@gmail.com>
2018-08-04 19:38:55 +0500

    fixed time format

--- src/main/java/su/kas/blog/web/PostController.java
+++ src/main/java/su/kas/blog/web/PostController.java
@@ -6,8 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import su.kas.blog.model.Post;
 import su.kas.blog.service.PostService;
 import su.kas.blog.web.exception.ResourceNotFoundException;
+
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequestMapping("/posts")
@@ -17,8 +20,10 @@ public class PostController {
 
     @GetMapping("/{id}")
     public String home(@PathVariable long id, Model model) {
-        model.addAttribute("post", postService.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found")));
+        Post post = postService.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        model.addAttribute("post", post);
+        model.addAttribute("formattedTime", post.getTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
         return "post";
     }
 }
